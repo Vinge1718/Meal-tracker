0c6e0c47b252d92a95b68a8ec8b29c92ca84eb9d
Victor King'e <vinge1718@gmail.com>
2017-09-26 14:14:28 +0300

    connect the files responsible for displaying the meals

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,22 +6,20 @@ import { Meal } from "./meal.model"
   template: `
   <div class="container">
     <h1 class="jumbotron">Meal Tracker</h1>
-    <h3>Your Meals so Far Today</h3><hr>
+    <h3>An Application to Track your Daily Meals </h3><hr>
     <div class="row">
       <div class="col-md-4">
-        <div *ngFor="let currentMeal of meals">
-          <p>Name of your meal: {{currentMeal.name}}</p>
-          <p>Calorie Count: kcal.{{currentMeal.calories}}</p>
-          <p>Meal Description: {{currentMeal.description}}</p>
-          <button (click)="editThisMeal(currentMeal)">Edit</button>
-        </div>
+      <meal-list
+          [childMealList]="meals"
+          (clickSender)="editThisMeal($event)"
+        ></meal-list>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4"  *ngIf="selectedMeal">
         <h4>Change the meal details</h4>
         <p>Enter Meals Name: <input [(ngModel)]="selectedMeal.name"></p>
         <p>Fill in Calories: <input [(ngModel)]="selectedMeal.calories"></p>
         <p>Enter Description: <input [(ngModel)]="selectedMeal.description"></p>
-        <button *ngIf="" (click)="saveBottonClick()">Save Changes</button>
+        <button (click)="saveButtonClick()">Save Changes</button>
       </div>
       <div class="col-md-4"></div>
     </div>
@@ -32,7 +30,11 @@ import { Meal } from "./meal.model"
 export class AppComponent {
   public meals: Meal[] = [ new Meal("chips",555,"ndondo") ];
 
-  selectedMeal: Meal = this.meals[0];
+  saveButtonClick(){
+    this.selectedMeal = null;
+  }
+
+  selectedMeal: Meal = null;
   editThisMeal(clickedMeal: Meal){
     this.selectedMeal=clickedMeal;
   }
