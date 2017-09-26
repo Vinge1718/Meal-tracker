40cd97e0fdfab8e116ffd17c5e760d55f60fd0d0
Victor King'e <vinge1718@gmail.com>
2017-09-26 10:32:27 +0300

    added the root typescript file

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1 class="jumbotron">Meal Tracker</h1>
+    <div clas="col-md-4"></div>
+    <div clas="col-md-4"></div>
+    <div clas="col-md-4"></div>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
