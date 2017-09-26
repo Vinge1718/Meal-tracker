cdff2688ede51b3860b82c9b52becf506e21c8ac
Victor King'e <vinge1718@gmail.com>
2017-09-26 16:02:54 +0300

    include the new meal component file

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
-import { FormsModule } from "@angular/forms"
+import { FormsModule } from "@angular/forms";
+import { MealListComponent } from "./meal-list.component";
+import { MealEditComponent } from "./edit-meal.component";
+import { NewMealComponent } from "./new-meal.component"
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    MealListComponent,
+    MealEditComponent,
+    NewMealComponent
+   ],
   bootstrap: [AppComponent]
 })
 
