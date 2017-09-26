38f311b58f25d0e3dcc1b0e7172a3ac9b7467766
Victor King'e <vinge1718@gmail.com>
2017-09-26 13:07:18 +0300

    add corresponding javascript files compiiled from their typescript versions

---
+++ app/meal.model.js
@@ -0,0 +1,11 @@
+"use strict";
+var Meal = (function () {
+    function Meal(name, calories, description) {
+        this.name = name;
+        this.calories = calories;
+        this.description = description;
+    }
+    return Meal;
+}());
+exports.Meal = Meal;
+//# sourceMappingURL=meal.model.js.map
