d332a7aa277b87036091c3db36c3fb9f9ccd4fcf
Victor King'e <vinge1718@gmail.com>
2017-09-26 15:28:31 +0300

    add the input and output class definitions necessary to connect the edit-task component and the root component

---
+++ app/edit-meal.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter} from "@angular/core";
+import { Meal } from "./meal.model";
+
+@Component({
+  selector:"meal-edit",
+  template:`
+    <div class="col-sm-4"  *ngIf="childSelectedMeal">
+      <h4>Change the meal details</h4>
+      <p>Enter Meals Name: <input [(ngModel)]="childSelectedMeal.name"></p>
+      <p>Fill in Calories: <input [(ngModel)]="childSelectedMeal.calories"></p>
+      <p>Enter Description: <input [(ngModel)]="childSelectedMeal.description"></p>
+      <button (click)="saveButtonClick()">Save Changes</button>
+    </div>
+`
+})
+
+export class MealEditComponent {
+  @Input() childSelectedMeal: Meal;
+  @Output() saveEditsClickSender = new EventEmitter();
+  saveButtonClick(){
+    this.saveEditsClickSender.emit();
+  }
+}
