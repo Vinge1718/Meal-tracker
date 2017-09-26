cd7ad614f5a0b113a02686c0996628cac7d58c8f
Victor King'e <vinge1718@gmail.com>
2017-09-26 16:03:51 +0300

    connect the new meal component file to the root file

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,27 +8,28 @@ import { Meal } from "./meal.model"
     <h1 class="jumbotron">Meal Tracker</h1>
     <h3>An Application to Track your Daily Meals </h3><hr>
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-sm-4">
       <meal-list
           [childMealList]="meals"
           (clickSender)="editThisMeal($event)"
         ></meal-list>
       </div>
-      <div class="col-md-4"  *ngIf="selectedMeal">
-        <h4>Change the meal details</h4>
-        <p>Enter Meals Name: <input [(ngModel)]="selectedMeal.name"></p>
-        <p>Fill in Calories: <input [(ngModel)]="selectedMeal.calories"></p>
-        <p>Enter Description: <input [(ngModel)]="selectedMeal.description"></p>
-        <button (click)="saveButtonClick()">Save Changes</button>
+      <meal-edit
+          [childSelectedMeal]="selectedMeal"
+          (saveEditsClickSender)="saveButtonClick()"
+        ></meal-edit>
+      <div class="col-sm-4">
+        <new-meal
+
+          ></new-meal>
       </div>
-      <div class="col-md-4"></div>
     </div>
   </div>
   `
 })
 
 export class AppComponent {
-  public meals: Meal[] = [ new Meal("chips",555,"ndondo") ];
+  public meals: Meal[] = [  ];
 
   saveButtonClick(){
     this.selectedMeal = null;
